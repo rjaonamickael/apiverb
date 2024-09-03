efa2f45cdc02ced31b57b5d9acdc13e0f8c57a08
Mickaël <r.jaonamickael@fmail.com>
2024-09-02 22:26:26 -0400

    derner commit

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -8,10 +8,6 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
@@ -23,15 +19,7 @@ import { CommonModule } from '@angular/common';
     MatFormFieldModule,
     CommonModule,  
   ],
-  imports: [
-    ReactiveFormsModule,
-    MatButtonModule,
-    MatInputModule,
-    MatFormFieldModule,
-    CommonModule,  
-  ],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit, OnDestroy {
@@ -41,64 +29,38 @@ export class LoginComponent implements OnInit, OnDestroy {
   private router = inject(Router);
   private usersService = inject(UsersService);
   private userSubscription: Subscription | null = null;
-  private userSubscription: Subscription | null = null;
 
   loginFormGroup = this.formBuilder.group({
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required, Validators.minLength(6)]]
-    password: ['', [Validators.required, Validators.minLength(6)]]
   });
 
-  userLoginView: User = Object.assign(new User(), this.loginFormGroup.value);
   userLoginView: User = Object.assign(new User(), this.loginFormGroup.value);
 
   invalidCredentials = false;
 
   login(): void {
-  login(): void {
     this.usersService.login(this.loginFormGroup.value as Credentials).subscribe({
-      next: (result: User | null | undefined) => {
       next: (result: User | null | undefined) => {
         this.navigateHome();
       },
       error: error => {
         if (error.error === "Invalid Password.") {
-        if (error.error === "Invalid Password.") {
           this.invalidCredentials = true;
         } else {
           alert("Erreur inconnue");
-        }
+        } 
         console.log(error);
       }
-    });
-  }
     });
   }
 
   navigateHome() {
     this.router.navigate(['home']);
   }
-    this.router.navigate(['home']);
-  }
 
-
-  navigateRegister() {
-    this.router.navigate(['register']);
-  }
 
   ngOnInit(): void {
-    this.userSubscription = this.loginFormGroup.valueChanges.subscribe(() => {
-      this.userLoginView = Object.assign(new User(), this.loginFormGroup.value);
-    });
-
-    this.loginFormGroup.get('email')?.valueChanges.subscribe(() => {
-      this.invalidCredentials = false;
-    });
-
-    this.loginFormGroup.get('password')?.valueChanges.subscribe(() => {
-      this.invalidCredentials = false;
-    });
-  }
     this.userSubscription = this.loginFormGroup.valueChanges.subscribe(() => {
       this.userLoginView = Object.assign(new User(), this.loginFormGroup.value);
     });
