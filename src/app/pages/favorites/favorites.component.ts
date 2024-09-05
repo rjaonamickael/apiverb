2c12a6db40e24c6ad2279d8dcbc7b9fe746f4487
Mickaël <r.jaonamickael@fmail.com>
2024-09-04 22:12:52 -0400

    getAllfavorites OK reste les styles

--- src/app/pages/favorites/favorites.component.ts
+++ src/app/pages/favorites/favorites.component.ts
@@ -1,23 +1,31 @@
 
 import { Component, OnInit } from '@angular/core';
 import { verbFunctions } from '../../functions/verb.functions';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-favorites',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './favorites.component.html',
   styleUrl: './favorites.component.scss'
 })
 export class FavoritesComponent implements OnInit {
 
-
+  listVerbFavorites:any;
   private functions = new verbFunctions();
 
-  ngOnInit(): void {
-    this.functions.getAllFavorites();
+  async getAllFavorites():Promise <void> {
+    this.listVerbFavorites = await this.functions.getAllFavorites();
     console.log("ok");
+    console.log(this.listVerbFavorites);
   }
+
+  ngOnInit():void {
+    this.getAllFavorites();
+  }
+
+
 
   
 
